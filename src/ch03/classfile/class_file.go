ea0dbf416b64d2553122f051772579d816d51099
yaphone <zhouyaphone@163.com>
2017-07-17 22:39:34 +0800

    3.2.8

--- src/ch03/classfile/class_file.go
+++ src/ch03/classfile/class_file.go
@@ -4,7 +4,7 @@ import "fmt"
 
 type ClassFile struct {
 	//magic uint32
-	minorVersion uint16 //新生代？
+	minorVersion uint16
 	majorVersion uint16
 	constantPool ConstantPool //常量池
 	accessFlags uint16
@@ -46,11 +46,25 @@ func (self *ClassFile) read(reader *ClassReader) {
 }
 
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader){
-	//return self.magic
+	magic := reader.readUint32();
+	if magic != 0XCAFEBABE {
+		panic("java.lang.ClassFormatError: magic")
+	}
 }
 
-func (sele *ClassFile) readAndCheckVersion(reader *ClassReader) {
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0{
+			return
+		}
 
+	}
+	panic("java.lang.UnsupportedClassVersionError")
 }
 
 func (self *ClassFile) MinorVersion() uint16{
